5c54716719ccc7eceb1ae6abcfacafed7317832a
gigano01 <noahvanleemput@hotmail.com>
2024-12-05 07:28:04 +0100

    start of day 5 - need to go to docter first :c

---
+++ day5.ts
@@ -0,0 +1,27 @@
+import { announceChallenge, decodeFile } from './common';
+
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+announceChallenge(1, "Historian Hysteria")
+
+async function part1() {
+	
+	console.log("solution part 1:", "NAN");
+}
+
+async function part2() {
+	
+	
+	console.log("solution part 2:", "NAN");
+}
+
+async function main () {
+	await decodeFile(filePath, async (lines) => {
+		
+    });
+
+	part1();
+	part2();
+}
+
+main()
