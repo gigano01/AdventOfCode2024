2d15c607d4611c07e8418f3a1e2ef4fff8a815c0
gigano01 <noahvanleemput@hotmail.com>
2024-12-05 07:24:16 +0100

    updated template

--- template.ts
+++ template.ts
@@ -5,20 +5,23 @@ const filePath = process.argv[2];
 announceChallenge(1, "Historian Hysteria")
 
 async function part1() {
-	await decodeFile(filePath, async (lines) => {
-
-    });
 
 	console.log("solution part 1:", "NAN");
 }
 
 async function part2() {
-	await decodeFile(filePath, async (lines) => {
-		
-    });
+	
 	
 	console.log("solution part 2:", "NAN");
 }
 
-part1();
-part2();
+async function main () {
+	await decodeFile(filePath, async (lines) => {
+		
+    });
+
+	part1();
+	part2();
+}
+
+main()
