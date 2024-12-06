8f502295fe37710edf7a5e7ca54ff804342729d7
gigano01 <noahvanleemput@hotmail.com>
2024-12-06 15:21:14 +0100

    this somehow gives a wrong answer

--- day6.ts
+++ day6.ts
@@ -26,8 +26,60 @@ function prettyPrintGrid(grid: string[][]){
 	}
 }
 
-function rotateDirection(dirX, dirY) {
+/*
+-1 0
+0 1
+1 0
+0 -1
+-1 0
+*/
+function rotateDirection(dirX: number, dirY: number): [number, number] {
+	// console.log(dirX,dirY)
+	if(dirX == 0){
+		// console.log("H")
+		dirX = -dirY
+		dirY = 0
+	} else {
+		dirY = dirX
+		dirX = 0
+	}
+	// console.log(dirX,dirY)
+	return [dirX, dirY]
 	
+}
+//1454
+function simulate(grid: string[][], x: number, y: number){
+	
+	let directionY = -1;
+	let directionX = 0;
+	let noEdgeKiss = true;
+	let tries = 0
+	while(noEdgeKiss){
+		tries++;
+		// if(tries > 10000) return true
+		// console.log(x,y)
+
+		if(y >= grid.length || y < 0 ) {noEdgeKiss = false; return false};
+		if(x < 0 || x >= grid[y].length) {noEdgeKiss = false; return false}
+		if(Number.isNaN(parseInt(grid[y][x]))) {
+			grid[y][x] = "0"
+		} else {
+			if(grid[y][x] == "1000"){
+				console.log(grid)
+				return true;
+			}
+			grid[y][x] = String(parseInt(grid[y][x]) + 1)
+		}
+		x += directionX
+		y += directionY
+		if(grid[y+directionY]?.[x+directionX] === "#"){
+			// console.log("turn!");
+			[directionX, directionY] = rotateDirection(directionX, directionY)
+			// console.log(directionX, directionY)
+		}
+
+
+	}
 }
 
 async function part1(grid: string[][]) {
@@ -35,33 +87,58 @@ async function part1(grid: string[][]) {
 	let directionY = -1;
 	let directionX = 0;
 	let {x, y} = findStart(grid)
-	console.log(x, y)
+	// console.log(x, y)
+	let count = 0;
 	while(noEdgeKiss){
-		console.log(x,y)
-		if(y > grid.length || y < 0 || x < 0 || x > grid[y].length) {noEdgeKiss = false; continue};
-		grid[y][x] = "X"
+		// console.log(x,y)
+
+		if(y >= grid.length || y < 0 ) {noEdgeKiss = false; continue};
+		if(x < 0 || x > grid[y].length) {noEdgeKiss = false; continue}
+		if(grid[y][x] != "X"){
+			count++
+			grid[y][x] = "X"
+		}
 		x += directionX
 		y += directionY
 		if(grid[y+directionY]?.[x+directionX] === "#"){
-			console.log("turn!")
+			// console.log("turn!");
+			[directionX, directionY] = rotateDirection(directionX, directionY)
+			// console.log(directionX, directionY)
 		}
 
 
 	}
-    console.log("solution part 1:", "NAN");
+    console.log("solution part 1:", count);
 }
 
-async function part2() {
-    console.log("solution part 2:", "NAN");
+async function part2(grid: string[][]) {
+	let {x, y} = findStart(grid);
+	let count = 0
+	for(let i = 0; i < grid.length; i++){
+		for(let j = 0; j < grid[i].length; j++){
+			let fonyGrid: string[][] = structuredClone(grid)
+			if(fonyGrid[i][j] != "#" && fonyGrid[i][j] != "^"){
+				// console.log(i,j)
+				fonyGrid[i][j] = "#"
+				// console.log(fonyGrid)
+				if(simulate(fonyGrid, x, y)){
+					// console.log(i,j, fonyGrid)
+					count++;
+					console.log(count)
+				}
+			}
+		}
+	}
+    console.log("solution part 2:", count);
 }
 
 async function main() {
     const lines = Deno.readTextFileSync(filePath).trim().split("\n");
     const grid: string[][] = lines.map((value) => {return value.trim().split("");});
-	prettyPrintGrid(grid)
+	// prettyPrintGrid(grid)
 	// console.log(grid)
-    part1(grid);
-    part2();
+    part1(structuredClone(grid));
+    part2(structuredClone(grid));
 }
 
 main();
