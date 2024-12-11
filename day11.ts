61463e48a75d3c59e9e879bc2b9513f6b38913c5
gigano01 <noahvanleemput@hotmail.com>
2024-12-11 18:19:49 +0100

    day 11 finished (at least should be)

--- day11.ts
+++ day11.ts
@@ -1,97 +1,109 @@
-import { announceChallenge } from './common.ts';
-import { LinkedList, LinkedListItem } from "https://deno.land/x/linkedlist@v1.1.1/mod.ts"
+import { announceChallenge } from "./common.ts";
+import { LinkedList, LinkedListItem } from "https://deno.land/x/linkedlist@v1.1.1/mod.ts";
 
 // Get the file path from command line arguments
 const filePath = Deno.args[0] || "input/day11.test";
-announceChallenge(11, "Plutonian Pebbles")
+announceChallenge(11, "Plutonian Pebbles");
 
 async function part1(stones: number[]) {
-	const maxBlinks = 25
-	for(let blink = 0; blink < maxBlinks; blink++){
-		const newArray = []
-		for(let i = 0; i < stones.length; i++){
-			const stone = stones[i];
+    const maxBlinks = 25;
+    for (let blink = 0; blink < maxBlinks; blink++) {
+        const newArray = [];
+        for (let i = 0; i < stones.length; i++) {
+            const stone = stones[i];
 
-			if(stone == 0){
-				newArray.push(1)
-			} else if (String(stone).length % 2 == 0) { //even digit length
-				const str = String(stone)
-				
-				newArray.push(parseInt(str.slice(0,str.length/2)))
-				newArray.push(parseInt(str.slice(str.length/2, str.length)))
-			} else {
-				newArray.push(stone * 2024)
-			}
-		}
-		stones = newArray
-		// console.log(blink,stones)
-	}
-	console.log("solution part 1:", stones.length);
+            if (stone == 0) {
+                newArray.push(1);
+            } else if (String(stone).length % 2 == 0) {
+                //even digit length
+                const str = String(stone);
+
+                newArray.push(parseInt(str.slice(0, str.length / 2)));
+                newArray.push(parseInt(str.slice(str.length / 2, str.length)));
+            } else {
+                newArray.push(stone * 2024);
+            }
+        }
+        stones = newArray;
+        // console.log(blink,stones)
+    }
+    console.log("solution part 1:", stones.length);
 }
 
-function splitDigits(number: number){
-	const digitCount = Math.floor(Math.log10(number)+1);
-	const base = Math.pow(10,digitCount/2)
-	// console.log(base)
-	const digit1 = number % base
-	const digit2 = (number-digit1) / base
-	return [digit1,digit2]	
+function splitDigits(number: number) {
+    const digitCount = Math.floor(Math.log10(number) + 1);
+    const base = Math.pow(10, digitCount / 2);
+    // console.log(base)
+    const digit1 = number % base;
+    const digit2 = (number - digit1) / base;
+    return [digit1, digit2];
 }
 
-function memoizePerformActions(){
-	let cache = {}
-	return (stone: number):number[]=>{
-		if(stone in cache){
-			return cache[stone]
-		}
+function memoizePerformActions() {
+    let cache = {};
+    return (stone: number): number[] => {
+        if (stone in cache) {
+			// console.log(stone)
+            return cache[stone];
+        }
 
-		if(stone == 0){
-			const a = [1]
-			cache[stone] = a
-			return a
-			// stones.push(1)
-		} else if ((Math.floor(Math.log10(stone)+1)) % 2 == 0) { //even digit length
-			const res = splitDigits(stone)
-			cache[stone] = res
-			return res
-			// stones.push(digits[1])
-			// stones.push(digits[0])
-		} else {
-			const res = [stone * 2024]
-			cache[stone] = res
-			return res
-			// stones.push(stone * 2024)
-		}
-	}
+        if (stone == 0) {
+            const a = [1];
+            cache[stone] = a;
+            return a;
+            // stones.push(1)
+        } else if (Math.floor(Math.log10(stone) + 1) % 2 == 0) {
+            //even digit length
+            const res = splitDigits(stone);
+            cache[stone] = res;
+            return res;
+            // stones.push(digits[1])
+            // stones.push(digits[0])
+        } else {
+            const res = [stone * 2024];
+            cache[stone] = res;
+            return res;
+            // stones.push(stone * 2024)
+        }
+    };
 }
 
-async function part2(stones: LinkedList<number>) {
-	const maxBlinks = 75
-	const performActions = memoizePerformActions()
-	for(let blink = 0; blink < maxBlinks; blink++){
-		const len = stones.length
-		stones.forEach((stone,i)=>{
-			// console.log(Math.floor(Math.log10(stone)+1), stone)
-			const H = performActions(stone)
-			i.value = H[0]
-			if(H[1]){
-				stones.push(H[1])
-			}
-		})
-		
-		// console.log(blink,stones)
-		console.log(blink, stones.length)
-	}
+let glob = 0
+const performActions = memoizePerformActions();
+function blink(stone: number, blinkcount: number, counter: number) {
+	glob++
+	if(blinkcount >= 75) {glob--;return counter + 1}
 	
-	console.log("solution part 2:", stones.length);
+	const H = performActions(stone)
+	blinkcount += 1
+	counter = blink(H[0], blinkcount, counter)
+	if(H[1])counter = blink(H[1],blinkcount, counter)
+		
+		glob--
+		// console.log(glob)
+	return counter
 }
 
-async function main () {
-	const lines = Deno.readTextFileSync(filePath).trim().split("\n");
-	const stones: number[] = lines[0].trim().split(" ").map(val=>parseInt(val))
 
-	part1(structuredClone(stones));
-	part2(new LinkedList(stones));
+async function part2(stones: number[]) {
+	let count = 0
+	for(let i = 0; i < stones.length; i++){
+		count += blink(stones[i], 0, 0)
+		console.log(count)
+	}
+
+    console.log("solution part 2:", count);
 }
 
-main()
+async function main() {
+    const lines = Deno.readTextFileSync(filePath).trim().split("\n");
+    const stones: number[] = lines[0]
+        .trim()
+        .split(" ")
+        .map((val) => parseInt(val));
+
+    part1(structuredClone(stones));
+    part2(structuredClone(stones));
+}
+
+main();
